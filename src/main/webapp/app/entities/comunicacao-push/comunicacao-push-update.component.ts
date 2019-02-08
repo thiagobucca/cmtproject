f2fb2ea876e51f981a8de191a4d2848fc875ea28
hugo humberto rodrigues gonçalves <hugo79@hotmail.com>
2019-02-08 09:11:49 -0200

    correções

--- src/main/webapp/app/entities/comunicacao-push/comunicacao-push-update.component.ts
+++ src/main/webapp/app/entities/comunicacao-push/comunicacao-push-update.component.ts
@@ -5,6 +5,11 @@ import { Observable } from 'rxjs';
 
 import { IComunicacaoPush } from 'app/shared/model/comunicacao-push.model';
 import { ComunicacaoPushService } from './comunicacao-push.service';
+import { IComunicacaoPushLoja, ComunicacaoPushLoja } from 'app/shared/model/comunicacao-push-loja.model';
+import { ComunicacaoPushLojaService } from 'app/entities/comunicacao-push-loja';
+import { JhiAlertService } from 'ng-jhipster';
+import { ILojaMaconica } from 'app/shared/model/loja-maconica.model';
+import { LojaMaconicaService } from 'app/entities/loja-maconica';
 
 @Component({
     selector: 'jhi-comunicacao-push-update',
@@ -13,14 +18,39 @@ import { ComunicacaoPushService } from './comunicacao-push.service';
 export class ComunicacaoPushUpdateComponent implements OnInit {
     comunicacaoPush: IComunicacaoPush;
     isSaving: boolean;
-
-    constructor(private comunicacaoPushService: ComunicacaoPushService, private activatedRoute: ActivatedRoute) {}
+    lojas: ILojaMaconica[];
+    pushLoja: IComunicacaoPushLoja[];
+    lojasSelecionadas: any[];
+    constructor(
+        private comunicacaoPushService: ComunicacaoPushService,
+        private activatedRoute: ActivatedRoute,
+        private lojaMaconicaService: LojaMaconicaService,
+        private comunicacaoPushLojaService: ComunicacaoPushLojaService,
+        private jhiAlertService: JhiAlertService
+    ) {
+        this.lojasSelecionadas = [];
+    }
 
     ngOnInit() {
         this.isSaving = false;
         this.activatedRoute.data.subscribe(({ comunicacaoPush }) => {
             this.comunicacaoPush = comunicacaoPush;
+            this.comunicacaoPushLojaService.findByIdPush(this.comunicacaoPush.id).subscribe(
+                (res: HttpResponse<IComunicacaoPushLoja[]>) => {
+                    this.pushLoja = res.body;
+                    this.pushLoja.forEach(element => {
+                        this.lojasSelecionadas.push(element.lojaMaconicaId);
+                    });
+                },
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
         });
+        this.lojaMaconicaService.findByStatus(true).subscribe(
+            (res: HttpResponse<ILojaMaconica[]>) => {
+                this.lojas = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
     }
 
     previousState() {
@@ -37,7 +67,30 @@ export class ComunicacaoPushUpdateComponent implements OnInit {
     }
 
     private subscribeToSaveResponse(result: Observable<HttpResponse<IComunicacaoPush>>) {
-        result.subscribe((res: HttpResponse<IComunicacaoPush>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
+        result.subscribe(
+            (res: HttpResponse<ILojaMaconica>) => {
+                let dados = res.body;
+                if (this.lojasSelecionadas != null && this.lojasSelecionadas.length > 0) {
+                    if (this.pushLoja != null && this.pushLoja.length > 0) {
+                        this.pushLoja.forEach(element => {
+                            if (element.id !== undefined) {
+                                this.comunicacaoPushService.delete(element.id);
+                            }
+                        });
+                    }
+                    this.lojasSelecionadas.forEach(element => {
+                        let comunicacao = new ComunicacaoPushLoja();
+                        comunicacao.comunicacaoPushId = dados.id;
+                        comunicacao.lojaMaconicaId = element;
+                        element.lojaMaconicaId = dados.id;
+
+                        this.comunicacaoPushService.create(comunicacao);
+                    });
+                }
+                this.onSaveSuccess();
+            },
+            (res: HttpErrorResponse) => this.onSaveError()
+        );
     }
 
     private onSaveSuccess() {
@@ -47,5 +100,11 @@ export class ComunicacaoPushUpdateComponent implements OnInit {
 
     private onSaveError() {
         this.isSaving = false;
+    }
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+    trackById(index: number, item: any) {
+        return item.id;
     }
 }
