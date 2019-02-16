a6f771b8900292960742f567cc02ee5ef4def4d2
cmtdev <leonardofelix2004@hotmail.com>
2019-02-15 23:37:37 -0200

    Corrigido problemas gerados no merge com a branch leo-novosAjustes

--- src/main/java/com/cmt/myapp/web/rest/CupomResource.java
+++ src/main/java/com/cmt/myapp/web/rest/CupomResource.java
@@ -140,13 +140,8 @@ public class CupomResource {
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/cupoms");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
-<<<<<<< HEAD
-=======
-   
->>>>>>> bcb62b2ee8df9d6b77419eda5a6207beab1c00ac
-
-
-           /**
+    
+    /**
      * GET /users : get all users.
      *
      * @param pageable the pagination information
