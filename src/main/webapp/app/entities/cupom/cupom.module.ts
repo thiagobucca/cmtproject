bc9c715481d0cc01a5f907ac4ff5544e0e1df004
hugo humberto rodrigues gonçalves <hugo79@hotmail.com>
2019-02-23 11:26:28 -0300

    mascara cupom R$

--- src/main/webapp/app/entities/cupom/cupom.module.ts
+++ src/main/webapp/app/entities/cupom/cupom.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { NgxCurrencyModule } from 'ngx-currency';
 import { CmtprojectSharedModule } from 'app/shared';
 import {
     CupomComponent,
@@ -12,10 +12,22 @@ import {
     cupomPopupRoute
 } from './';
 
+export const customCurrencyMaskConfig = {
+    align: 'right',
+    allowNegative: false,
+    allowZero: false,
+    decimal: ',',
+    precision: 2,
+    prefix: 'R$ ',
+    suffix: '',
+    thousands: '.',
+    nullable: true
+};
+
 const ENTITY_STATES = [...cupomRoute, ...cupomPopupRoute];
 
 @NgModule({
-    imports: [CmtprojectSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [CmtprojectSharedModule, RouterModule.forChild(ENTITY_STATES), NgxCurrencyModule.forRoot(customCurrencyMaskConfig)],
     declarations: [CupomComponent, CupomDetailComponent, CupomUpdateComponent, CupomDeleteDialogComponent, CupomDeletePopupComponent],
     entryComponents: [CupomComponent, CupomUpdateComponent, CupomDeleteDialogComponent, CupomDeletePopupComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
