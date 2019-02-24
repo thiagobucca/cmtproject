2c62cd7db07719eae420ce42b8852a52a165f78d
hugo humberto rodrigues gonçalves <hugo79@hotmail.com>
2019-02-24 11:51:27 -0300

    padronização layout

--- src/main/webapp/app/entities/estabelecimento-comercial/estabelecimento-comercial.component.ts
+++ src/main/webapp/app/entities/estabelecimento-comercial/estabelecimento-comercial.component.ts
@@ -10,6 +10,9 @@ import { Principal } from 'app/core';
 import { ITEMS_PER_PAGE } from 'app/shared';
 import { EstabelecimentoComercialService } from './estabelecimento-comercial.service';
 
+import { AuxiliarService } from 'app/shared/services/auxiliar.service';
+import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
     selector: 'jhi-estabelecimento-comercial',
     templateUrl: './estabelecimento-comercial.component.html'
@@ -38,7 +41,9 @@ export class EstabelecimentoComercialComponent implements OnInit, OnDestroy {
         private activatedRoute: ActivatedRoute,
         private dataUtils: JhiDataUtils,
         private router: Router,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private auxService: AuxiliarService,
+        private ref: ChangeDetectorRef
     ) {
         this.itemsPerPage = ITEMS_PER_PAGE;
         this.routeData = this.activatedRoute.data.subscribe(data => {
@@ -48,8 +53,14 @@ export class EstabelecimentoComercialComponent implements OnInit, OnDestroy {
             this.predicate = data.pagingParams.predicate;
         });
     }
-
+    get loading(): boolean {
+        return this.auxService.isLoading;
+    }
+    set loading(status: boolean) {
+        this.auxService.isLoading = status;
+    }
     loadAll() {
+        this.loading = true;
         this.estabelecimentoComercialService
             .query({
                 page: this.page - 1,
@@ -57,11 +68,22 @@ export class EstabelecimentoComercialComponent implements OnInit, OnDestroy {
                 sort: this.sort()
             })
             .subscribe(
-                (res: HttpResponse<IEstabelecimentoComercial[]>) => this.paginateEstabelecimentoComercials(res.body, res.headers),
-                (res: HttpErrorResponse) => this.onError(res.message)
+                (res: HttpResponse<IEstabelecimentoComercial[]>) => {
+                    this.paginateEstabelecimentoComercials(res.body, res.headers);
+                    this.loading = false;
+                    this.ref.detectChanges();
+                },
+                (res: HttpErrorResponse) => {
+                    this.onError(res.message);
+                    this.loading = false;
+                    this.ref.detectChanges();
+                }
             );
     }
-
+    detalhar(parametros: []) {
+        this.loading = true;
+        this.router.navigate(parametros);
+    }
     loadPage(page: number) {
         if (page !== this.previousPage) {
             this.previousPage = page;
