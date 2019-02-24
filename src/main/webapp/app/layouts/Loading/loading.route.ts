f7319b723955afb535be2e6479b7268d945af50d
hugo humberto rodrigues gonçalves <hugo79@hotmail.com>
2019-02-24 12:04:56 -0300

    loading

--- src/main/webapp/app/layouts/Loading/loading.route.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-import { LoadingComponent } from './loading.component';
-
-export const loadingRoute: Route = {
-    path: '',
-    component: LoadingComponent,
-    outlet: 'loading'
-};
