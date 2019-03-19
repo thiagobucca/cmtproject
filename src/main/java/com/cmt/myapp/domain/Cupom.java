d495e1516767c378b7a4eb698376ce638a0c4792
cmtdev <llaercio.lima@gmail.com>
2019-03-19 09:21:37 -0300

    alteracao status

--- src/main/java/com/cmt/myapp/domain/Cupom.java
+++ src/main/java/com/cmt/myapp/domain/Cupom.java
@@ -237,7 +237,7 @@ public class Cupom implements Serializable {
             ", fotoContentType='" + getFotoContentType() + "'" +
             ", estabelecimentoComercialId=" + getEstabelecimentoComercialId() +
             ", usuarioId=" + getUsuarioId() +
-            ", status='" + getStatus() + "'" +
+            ", status='" + getStatusCupom() + "'" +
             "}";
     }
 }
